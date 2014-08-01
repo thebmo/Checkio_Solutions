258f0ce87f5d304a3e37f40269760d8f0ec1fc45
thebmo <bmosier@gmail.com>
2014-08-01 19:20:57 -0400

    solved Min_max, no importing allowed

--- min_max.py
+++ min_max.py
@@ -1,22 +1,38 @@
 def min(*args, **kwargs):
     key = kwargs.get("key", None)
-    min = 100000
-    for item in args:
-        if item < min:
-            min = item
-        
+    items = sorted(get_items(args), key=key)
+    min = items[0]
+    if key == int:
+        min = int_key(items, min)
     return min
 
-
 def max(*args, **kwargs):
-    key = kwargs.get("key", None)
-    max = 0
-    for item in args:
-        if item > max:
-            max = item
+    key = kwargs.get("key", None) 
+    items = sorted(get_items(args), key=key)
+    max = items[-1]
+    if key == int:
+        max = int_key(items, max)
     return max
+    
+def get_items(args):
+    items = []
+    if len(args) == 1:
+        for arg in args:
+            for a in arg:
+                items.append(a)
+    else:
+        for arg in args:
+            items.append(arg)
+    return items
 
-
+# I feel like this is a hack but dont know how else to
+# handle the float -> int sorting
+def int_key(items, num):
+    for item in items:
+        if int(item) == int(num) and item < num:
+            num = item
+    return num
+    
 if __name__ == '__main__':
     #These "asserts" using only for self-checking and not necessary for auto-testing
     assert max(3, 2) == 3, "Simple case max"
