4006848674da1b0e83410aac09be620a8896f0f7
thebmo <bmosier@gmail.com>
2014-08-07 14:39:41 -0400

    made some logic changes to golden_pyramid

--- home/golden_pyramid.py
+++ home/golden_pyramid.py
@@ -1,113 +1,71 @@
-from math import pow as pow
+
 def count_gold(pyramid):
-    
-    # pos = 0
-    # gold = pyramid[0][pos]
-    # pyramid[0][pos] = 'x' # for testing with lists
     
     # initialize the val list
     vals = [ [0 for j in range(i+1)] for i in range(len(pyramid))]
     vals[0][0] = pyramid[0][0]
-    # check_vals(vals)
     
+    
+    # might be going about htis the wrong way (reverse order)
     for row in range(len(pyramid)):
         for col in range(len(pyramid[row])):            
-            print "row: %s | col: %s" %(row, col)
             if row!=0:
-            
-                
-                
-                if col >1 : # <<<<<<<<<< RIGHT HERE
-                    
-                    
-                    
+                if col+1 > len(pyramid[row-1]): # <<<<<<<<<< RIGHT HERE                    
+                    vals[row][col] += pyramid[row][col] + vals[row-1][col-1]
+                else:
                     if pyramid[row-1][col] > pyramid[row-1][col-1]:
-                        vals[row][col] = pyramid[row][col] + pyramid[row-1][col]
+                        vals[row][col] += pyramid[row][col] + vals[row-1][col]
                     else:
-                        vals[row][col] = pyramid[row][col] + pyramid[row-1][col-1]
+                        vals[row][col] += pyramid[row][col] + vals[row-1][col-1]
+    
+    
     check_vals(vals)
+    return max(vals[len(vals)-1])
+    
 
-    
-    
-    
-    
-    
-    # ***************************************** #
-    
-    # for level in range(len(pyramid)):
-        # if level+1 < len(pyramid):
-            # if pos+1 < len(pyramid):
-                # if pyramid[level+1][pos] > pyramid[level+1][pos+1]:
-                    # gold+= pyramid[level+1][pos]
-                    # pyramid[level+1][pos] = 'x' # for testing with lists
-                    
-                # else:
-                    # gold+= pyramid[level+1][pos+1]
-                    # pyramid[level+1][pos+1] = 'x' # for testing with lists
-                    # pos+=1
-        
-        # # this prints each modified level
-        # lvl_str = ''
-        # for item in pyramid[level]:
-            # lvl_str+= str(item)
-        # print lvl_str
-        # *******************************
-
-    
-    # ***************************************** #
-    # print 'gold: %s' % gold
-    # print "Routes: %s" % (int(pow(2,(len(pyramid)-1))))
-    # return gold
 
 # verify the val list is populated
 def check_vals(vals):
     for level in vals:
         val_str = ''
         for val in level:
-            val_str+= str(val)
+            val_str+= str(val) +' '
         print val_str
 
+
 if __name__ == '__main__':
-    # #These "asserts" using only for self-checking and not necessary for auto-testing
-    # assert count_gold([
-        # [1,],
-        # [2, 3],
-        # [3, 3, 1],
-        # [3, 1, 5, 4],
-        # [3, 1, 3, 1, 3],
-        # [2, 2, 2, 2, 2, 2],
-        # [5, 6, 4, 5, 6, 4, 3]
-    # ])
-    assert count_gold([
-        [1,],
-        [2, 1],
-        [1, 2, 1],
-        [1, 2, 1, 1],
-        [1, 2, 1, 1, 1],
-        [1, 2, 1, 1, 1, 1],
-        [1, 2, 1, 1, 1, 1, 9]
-    ])
-    # assert count_gold((
-        # (1,),
-        # (2, 3),
-        # (3, 3, 1),
-        # (3, 1, 5, 4),
-        # (3, 1, 3, 1, 3),
-        # (2, 2, 2, 2, 2, 2),
-        # (5, 6, 4, 5, 6, 4, 3)
-    # )) == 23, "First example"
-    # assert count_gold((
-        # (1,),
-        # (2, 1),
-        # (1, 2, 1),
-        # (1, 2, 1, 1),
-        # (1, 2, 1, 1, 1),
-        # (1, 2, 1, 1, 1, 1),
-        # (1, 2, 1, 1, 1, 1, 9)
-    # )) == 15, "Second example"
-    # assert count_gold((
-        # (9,),
-        # (2, 2),
-        # (3, 3, 3),
-        # (4, 4, 4, 4)
-    # )) == 18, "Third example"
+    assert count_gold((
+        (1,),
+        (2, 3),
+        (3, 3, 1),
+        (3, 1, 5, 4),
+        (3, 1, 3, 1, 3),
+        (2, 2, 2, 2, 2, 2),
+        (5, 6, 4, 5, 6, 4, 3)
+    )) == 23, "First example"
+    assert count_gold((
+        (1,),
+        (2, 1),
+        (1, 2, 1),
+        (1, 2, 1, 1),
+        (1, 2, 1, 1, 1),
+        (1, 2, 1, 1, 1, 1),
+        (1, 2, 1, 1, 1, 1, 9)
+    )) == 15, "Second example"
+    assert count_gold((
+        (9,),
+        (2, 2),
+        (3, 3, 3),
+        (4, 4, 4, 4)
+    )) == 18, "Third example"
+    assert count_gold((
+        (6,),
+        (6,9),
+        (7,1,4),
+        (6,9,9,7),
+        (1,6,7,9,7),
+        (5,7,2,6,0,9),
+        (1,2,2,6,0,1,6),
+        (8,5,0,3,1,4,3,1),
+        (9,6,4,1,1,9,3,7,9),
+        (5,8,4,3,5,4,5,1,8,3))) == 66, "fourth example"
