12e3a2dd4d2eaacc4403732ab99ce5c201c8dcf5
thebmo <bmosier@gmail.com>
2015-02-08 19:36:19 -0500

    wirked on maze

--- home/open_labyrinth.py
+++ home/open_labyrinth.py
@@ -43,25 +43,35 @@ def main():
     
     print '\ninitial:', initial
     print '   goal:', goal
+    print 'Is Node?:', is_node(curr, visited, GRAPH, dirs)
     
-    starting_nodes = node_test(curr, GRAPH, visited, dirs)
-    print starting_nodes
-    temp_paths = []
-    for node in starting_nodes:
-        path = []
-        next = node
+    # master loop
+    while(True):
+        starting_nodes = node_test(curr, GRAPH, visited, dirs)
+        print 'starting nodes:', starting_nodes
         
-        while(True):
-            next_list = node_test(next, GRAPH, visited, dirs)
-            if len(next_list) == 1:
-                next = next_list[0]
-                path.append(next)
-            else:
-                break
+        # builds a list of possible paths
+        temp_paths = []
+        for node in starting_nodes:
+            path = []
+            next = node
             
+            while(True):
+                next_list = node_test(next, GRAPH, visited, dirs)
+                if len(next_list) == 1:
+                    next = next_list[0]
+                    path.append(next)
+                else:
+                    break
+            temp_paths.append(path)
             
-
-        print 'node:',node, 'path:', path
+        print 'temp paths:', temp_paths
+        print 'visited:', visited
+        
+        raw_input('Press Enter\n')
+        
+        if curr == goal:
+            print 'Goal Reached!'
         
     
 # returns the charecter of the direction traveled
@@ -69,19 +79,32 @@ def get_direction(dirs, curr, next):
     return dirs[(next[0] - curr [0], next[1] - curr [1])]
         
 
-# Tests a coord to see if it is  a node
+# Tests a coord to see if it open nodes next to it
 def node_test(curr, GRAPH, visited, dirs):
     nodes = []
-    visited.append(curr)
+    temp_visited = [v for v in visited]
+    temp_visited.append(curr)
     x = curr[0]
     y = curr[1]
     
     for d in dirs:
-        if GRAPH[x+d[0]][y+d[1]] == 0 and (x+d[0], y+d[1]) not in visited: 
+        if GRAPH[x+d[0]][y+d[1]] == 0 and (x+d[0], y+d[1]) not in temp_visited: 
                 nodes.append((x+d[0], y+d[1]))
-                visited.append(nodes[-1])
-    
+                temp_visited.append(nodes[-1])
     return nodes
+
+
+# returns true if POS is a node
+def is_node(curr, visited, GRAPH, dirs):
+    x = curr[0]
+    y = curr[1]
+    nodes = 0
+    for d in dirs:
+        if GRAPH[x+d[0]][y+d[1]] == 0:
+            nodes+=1
+            # nodes.append((x+d[0], y+d[1]))
+    
+    return True if nodes > 2 or curr == (1, 1) else False
     
 if __name__ == '__main__':
   main()
